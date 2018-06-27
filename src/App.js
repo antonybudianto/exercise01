ded799ba3edd4f9f7fb394f3da6e74c72fd87c63
Antony Budianto <antonybudianto@gmail.com>
2018-06-27 16:37:18 +0700

    Refactor folder structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react'
+import FormView from './containers/FormView/FormView'
 
-import FormBody from './components/FormBody/FormBody'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import { FORM_DATA } from './data/form'
 import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header title={FORM_DATA.title} />
-        <FormBody />
-        <Footer />
+        <FormView />
       </div>
     )
   }
---
+++ src/containers/FormView/FormView.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+import Header from './components/Header/Header'
+import FormBody from './components/FormBody/FormBody'
+import Footer from './components/Footer/Footer'
+import { FORM_DATA } from '../../data/form'
+
+class FormView extends Component {
+  render() {
+    return (
+      <div>
+        <Header title={FORM_DATA.title} />
+        <FormBody questions={FORM_DATA.questions} />
+        <Footer />
+      </div>
+    )
+  }
+}
+
+export default FormView
