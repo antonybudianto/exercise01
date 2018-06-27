92b308562925de7d166aab7260894f4d8bbcd545
Antony Budianto <antonybudianto@gmail.com>
2018-06-27 18:37:17 +0700

    validate initial data

--- src/components/Question/TextQuestion/TextQuestion.js
+++ src/components/Question/TextQuestion/TextQuestion.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react'
 
+function validateField(value, question) {
+  let valid = false
+  if (!question.is_required && value === '') {
+    valid = true
+  } else if (value.length >= question.min_char_length) {
+    valid = true
+  }
+  return valid
+}
+
 class TextQuestion extends Component {
+  componentDidMount() {
+    const { value, question } = this.props
+    const valid = validateField(value, question)
+    if (valid) {
+      this.props.onChange({ valid, value })
+    }
+  }
+
   handleChange = e => {
     const value = e.target.value
     const { question } = this.props
-    let valid = false
-    if (!question.is_required && value === '') {
-      valid = true
-    } else if (value.length >= question.min_char_length) {
-      valid = true
-    }
+    const valid = validateField(value, question)
     this.props.onChange({ valid, value })
   }
 
